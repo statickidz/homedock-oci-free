9032777e8d466a26fab8abc800322583040474f0
Adrian Barrio <statickidz@gmail.com>
2025-07-06 11:10:14 +0200

    refactor: improve output formatting for HomeDock dashboard access

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 output "homedock_dashboard" {
-  value = <<-EOT
-    HomeDock Dashboard URL: http://${oci_core_instance.homedock_main.public_ip}/
-    Username: user
-    Password: passwd
-
-    Note: Wait 3-5 minutes for HomeDock installation to complete before accessing.
-  EOT
+  value = join("\n", [
+    "HomeDock Dashboard URL: http://${oci_core_instance.homedock_main.public_ip}",
+    "Username: user",
+    "Password: passwd",
+    "",
+    "Note: Wait 3-5 minutes for HomeDock installation to complete before accessing."
+  ])
 }

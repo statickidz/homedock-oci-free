1f1d03be0a61b3394d0a4ae94de7a8b7cc52aa75
Adrian Barrio <statickidz@gmail.com>
2025-12-23 09:06:46 +0100

    feat: enhance homedock-main.sh with SSH setup and iptables configuration

--- bin/homedock-main.sh
+++ bin/homedock-main.sh
@@ -5,6 +5,34 @@ if [ "$EUID" -ne 0 ]; then
     exec sudo "$0" "$@"
 fi
 
+# Add ubuntu SSH authorized keys to the root user
+mkdir -p /root/.ssh
+cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
+chown root:root /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
+
+# Add ubuntu user to sudoers
+echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# OpenSSH
+apt install -y openssh-server
+systemctl status sshd
+
+# Permit root login
+sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+# Allow all traffic
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -F
+iptables --flush
+
+# Save iptables rules
+apt install -y netfilter-persistent
+netfilter-persistent save
+
 # Log everything
 exec > >(tee -a /var/log/homedock-install.log)
 exec 2>&1
@@ -314,34 +342,6 @@ fi
 
 # Change to the /opt/ directory
 cd /opt/
-
-# Add ubuntu SSH authorized keys to the root user
-mkdir -p /root/.ssh
-cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/
-chown root:root /root/.ssh/authorized_keys
-chmod 600 /root/.ssh/authorized_keys
-
-# Add ubuntu user to sudoers
-echo "ubuntu ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-# OpenSSH
-apt install -y openssh-server
-systemctl status sshd
-
-# Permit root login
-sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-systemctl restart sshd
-
-# Allow all traffic
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -F
-iptables --flush
-
-# Save iptables rules
-apt install -y netfilter-persistent
-netfilter-persistent save
 
 # Install HomeDockOS > pseudo-TTY
 echo "Installing HomeDock OS..."
